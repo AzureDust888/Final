a0a3f1f863d0823093844a755773ff99a9e76e22
Azure <azurexsx@gmail.com>
2022-08-24 16:09:06 +0300

    Commited base

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <iostream>
+using namespace std;
 
 using namespace sf;
 
@@ -24,8 +26,8 @@ String TileMap[H] = {
 "0                                                                                                                                                    0",
 "0                                                                                                                                                    0",
 "0                                                                                                                                                    0",
-"0                                       r                                                                                                            0",
-"0                                    r  r                                                                                                             0",
+"0                                                                                                                                                   0",
+"0                                         rrr                                                                                                        0",
 "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
 "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
 "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
@@ -71,8 +73,8 @@ public:
 		if (currentFrame > 3) currentFrame -= 3;
 
 
-		if (dx > 0) sprite.setTextureRect(IntRect(112 + 31 * int(currentFrame), 144, 16, 16));
-		if (dx < 0) sprite.setTextureRect(IntRect(112 + 31 * int(currentFrame) + 16, 144, -16, 16));
+		/*if (dx > 0) sprite.setTextureRect(IntRect(112 + 31 * int(currentFrame), 144, 16, 16));
+		if (dx < 0) sprite.setTextureRect(IntRect(112 + 31 * int(currentFrame) + 16, 144, -16, 16));*/
 
 
 		sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
@@ -126,33 +128,20 @@ public:
 	FloatRect rect;
 	Sprite sprite;
 	float currentFrame;
-	bool life;
 
 
 	void set(Texture& image, int x, int y)
 	{
 		sprite.setTexture(image);
-		rect = FloatRect(x, y, 16, 16);
+		sprite.setColor(Color::Red);
+		rect = FloatRect(x, y, 14, 14);
 
 		dx = 0.05;
 		currentFrame = 0;
-		life = true;
 	}
 
 	void update(float time)
 	{
-		rect.left += dx * time;
-
-		Collision();
-
-
-		currentFrame += time * 0.005;
-		if (currentFrame > 2) currentFrame -= 2;
-
-		sprite.setTextureRect(IntRect(18 * int(currentFrame), 0, 16, 16));
-		if (!life) sprite.setTextureRect(IntRect(58, 0, 16, 16));
-
-
 		sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
 
 	}
@@ -179,7 +168,52 @@ public:
 
 };
 
+class ENEMY_WALL
+{
 
+public:
+	float dx, dy;
+	FloatRect rect;
+	Sprite sprite;
+	float currentFrame;
+
+
+	void set(int x, int y)
+	{
+		sprite.setColor(Color::Red);
+		rect = FloatRect(x, y, 1, 15);
+
+		dx = 0.05;
+		currentFrame = 0;
+	}
+
+	void update(float time)
+	{
+		sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
+
+	}
+
+
+	void Collision()
+	{
+
+		for (int i = rect.top / 16; i < (rect.top + rect.height) / 16; i++)
+			for (int j = rect.left / 16; j < (rect.left + rect.width) / 16; j++)
+				if ((TileMap[i][j] == 'P') || (TileMap[i][j] == '0'))
+				{
+					if (dx > 0)
+					{
+						rect.left = j * 16 - rect.width; dx *= -1;
+					}
+					else if (dx < 0)
+					{
+						rect.left = j * 16 + 16;  dx *= -1;
+					}
+
+				}
+	}
+
+};
 
 int main()
 {
@@ -187,23 +221,35 @@ int main()
 	RenderWindow window(VideoMode(1000, 300), "SFML works!");
 
 	Texture tileSet;
-	tileSet.loadFromFile("Mario_Tileset.png");
+	tileSet.loadFromFile("Tile.jpg");
+
+	Texture Spike;
+	Spike.loadFromFile("spike.png");
+
+	Texture Cube;
+	Cube.loadFromFile("CubeJ.jpg");
+
+	Texture GameOver;
+	GameOver.loadFromFile("Name.png");
+
+	Texture buildcube;
+	buildcube.loadFromFile("Tile.jpg");
 
 
-	PLAYER Mario(tileSet);
-	ENEMY  enemy;
-	enemy.set(tileSet, 48 * 16, 13 * 16);
+	PLAYER Mario(Cube);
+	vector<ENEMY> EN(2);
+	for (int i = 0, k = 30; i < EN.size(); i++)
+	{
+		EN[i].set(Spike, k * 16, 13 * 16);
+		k--;
+	}
+	vector<ENEMY_WALL> Wall(3);
+	Wall[0].set(671, 13 * 16);
+	Wall[1].set(671, 12 * 16);
+	Wall[2].set(671, 11 * 16);
 
-	Sprite END(tileSet);
 	Sprite tile(tileSet);
-
-	/*SoundBuffer buffer;
-	buffer.loadFromFile("Jump.ogg");
-	Sound sound(buffer);
-
-	Music music;
-	music.openFromFile("Mario_Theme.ogg");
-	music.play();*/
+	tile.setColor(Color::Cyan);
 
 	Clock clock;
 
@@ -213,7 +259,7 @@ int main()
 		float time = clock.getElapsedTime().asMicroseconds();
 		clock.restart();
 
-		time = time / 650;  // здесь регулируем скорость игры
+		time = time / 600;  // здесь регулируем скорость игры
 
 		if (time > 20) time = 20;
 
@@ -224,32 +270,48 @@ int main()
 				window.close();
 		}
 
+		if (true)    Mario.dx = 0.1;
 
-		if (Keyboard::isKeyPressed(Keyboard::Left))    Mario.dx = -0.1;
-
-		if (Keyboard::isKeyPressed(Keyboard::Right))    Mario.dx = 0.1;
-
-		if (Keyboard::isKeyPressed(Keyboard::Up))	if (Mario.onGround) { Mario.dy = -0.27; Mario.onGround = false;  /*sound.play();*/ }
+		if (Keyboard::isKeyPressed(Keyboard::Space))	if (Mario.onGround) { Mario.dy = -0.20; Mario.onGround = false; }
+		if (Keyboard::isKeyPressed(Keyboard::Up))	if (Mario.onGround) { Mario.dy = -0.20; Mario.onGround = false; }
 
 
 
 		Mario.update(time);
-		enemy.update(time);
-
-
-		if (Mario.rect.intersects(enemy.rect))
+		for (int i = 0; i < EN.size(); i++)
 		{
-			if (enemy.life) {
-				if (Mario.dy > 0) { enemy.dx = 0; Mario.dy = -0.2; enemy.life = false; }
-				else
-				{
+			EN[i].update(time);
+		}
+		for (int i = 0; i < Wall.size(); i++)
+		{
+			Wall[i].update(time);
+		}
+		for (int i = 0; i < Wall.size(); i++)
+		{
+			if (Mario.rect.intersects(Wall[i].rect))
+			{
+				cout << "Game Over" << endl;
+				window.close();
+				system("pause");
+				return 1;
+			}
+		}
+		for (int i = 0; i < EN.size(); i++)
+		{
+			if (Mario.rect.intersects(EN[i].rect))
+			{
 
-				};
+				cout << "Game Over" << endl;
+				window.close();
+				system("pause");
+				return 1;
 			}
 		}
 
 
-		if (Mario.rect.left > 200) offsetX = Mario.rect.left - 200;           //смещение
+
+
+		if (Mario.rect.left > 200) offsetX = Mario.rect.left - 200;
 
 
 
@@ -259,7 +321,7 @@ int main()
 		for (int i = 0; i < H; i++)
 			for (int j = 0; j < W; j++)
 			{
-				if (TileMap[i][j] == 'P')  tile.setTextureRect(IntRect(143 - 16 * 3, 112, 16, 16));
+				if (TileMap[i][j] == 'P')  tile.setTextureRect(IntRect(0, 0, 16, 16));
 
 				if (TileMap[i][j] == 'k')  tile.setTextureRect(IntRect(143, 112, 16, 16));
 
@@ -269,13 +331,13 @@ int main()
 
 				if (TileMap[i][j] == 'g')  tile.setTextureRect(IntRect(0, 16 * 9 - 5, 3 * 16, 16 * 2 + 5));
 
-				if (TileMap[i][j] == 'G')  tile.setTextureRect(IntRect(145, 222, 222 - 145, 255 - 222));
+				if (TileMap[i][j] == 'G')  tile.setTextureRect(IntRect(0, 0, 16, 16));
 
 				if (TileMap[i][j] == 'd')  tile.setTextureRect(IntRect(0, 106, 74, 127 - 106));
 
 				if (TileMap[i][j] == 'w')  tile.setTextureRect(IntRect(99, 224, 140 - 99, 255 - 224));
 
-				if (TileMap[i][j] == 'r')  tile.setTextureRect(IntRect(143 - 32, 112, 16, 16));
+				if (TileMap[i][j] == 'r')  tile.setTextureRect(IntRect(0, 0, 16, 16));
 
 				if ((TileMap[i][j] == ' ') || (TileMap[i][j] == '0')) continue;
 
@@ -286,8 +348,14 @@ int main()
 
 
 		window.draw(Mario.sprite);
-		window.draw(enemy.sprite);
-
+		for (int i = 0; i < EN.size(); i++)
+		{
+			window.draw(EN[i].sprite);
+		}
+		for (int i = 0; i < Wall.size(); i++)
+		{
+			window.draw(Wall[i].sprite);
+		}
 		window.display();
 	}
 
