7365d9af37f8c4c3cbf88713c41d51c3b3997c04
Antikvarnyy <radomir.degterev2002@gmail.com>
2022-08-24 12:40:55 +0300

    Slava!

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <thread>
+#include <chrono>
 using namespace std;
 int main()
 {	
-	cout << "Hello\n";
+	cout << "Slava Ukraini\n";
 	sf::RenderWindow window(sf::VideoMode(800, 400), "This");
 	sf::Event e;
 	while (window.isOpen())
 	{
-		window.clear(sf::Color::Cyan);
+
+		window.clear(sf::Color::Yellow);
 		window.display();
+		this_thread::sleep_for(chrono::milliseconds(500));
 		while (window.pollEvent(e))
 		{
 			if (e.type == sf::Event::Closed)
 				window.close();
 		}
+
+		window.clear(sf::Color::Blue);
+		window.display();
+		this_thread::sleep_for(chrono::milliseconds(500));
 	}
 }
