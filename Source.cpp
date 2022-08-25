febccf9c5bab47b5b0eccdb44e6031c672bb936f
Azure <azurexsx@gmail.com>
2022-08-25 15:29:22 +0300

    Update master

--- Source.cpp
+++ Source.cpp
@@ -32,9 +32,9 @@ String TileMap[H] = {
 "0                                                                  r                                               c                     kkkkkkkkkkkkrrrrrrrrrrrrkkkkkkkkkrrrrrrrrrrrrkkkrrrr  cccc      1  c                                                                                              0",
 "0                                                              r   k                         11       rrrrrrrrrr                         kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk        cccccc                                                                                                 0",
 "0                              1      11      1        11rrr   k   k11                  1rrrrrrrrrr   kkkkkkkkkk                         kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk                             r111                                                                               0",
-"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
-"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
-"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP ",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP ",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP ",
 };
 
 
@@ -333,7 +333,7 @@ public:
 	{
 
 		sprite.setTexture(image);
-		rect = FloatRect(x, y, 4, 16 * 15);
+		rect = FloatRect(x, y, 16, 16 * 16);
 
 		dx = 0.05;
 		currentFrame = 0;
@@ -373,6 +373,11 @@ int main()
 	Texture Cube;
 	Cube.loadFromFile("CubeJ.png");
 
+	Texture portal;
+	portal.loadFromFile("Portal.png");
+	Portal P;
+	P.set(portal, 16*298, 1);
+
 	Texture GameOver;
 	GameOver.loadFromFile("GameOVER.png");
 	Sprite gover;
@@ -398,144 +403,144 @@ int main()
 		k--;
 	}
 	vector<ENEMY> EN(1);
-	for (int i = 0, k = 30; i < EN.size(); i++)
+	/*for (int i = 0, k = 30; i < EN.size(); i++)
 	{
 		EN[i].set(Spike, k * 16, 15 * 16);
 		k--;
-	}
+	}*/
 
 	vector<ENEMY> EN1(2);
-	for (int i = 0, k = 39; i < EN1.size(); i++)
+	/*for (int i = 0, k = 39; i < EN1.size(); i++)
 	{
 		EN1[i].set(Spike, k * 16, 15 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN2(1);
-	for (int i = 0, k = 46; i < EN2.size(); i++)
+	/*for (int i = 0, k = 46; i < EN2.size(); i++)
 	{
 		EN2[i].set(Spike, k * 16, 15 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN3(2);
-	for (int i = 0, k = 56; i < EN3.size(); i++)
+	/*for (int i = 0, k = 56; i < EN3.size(); i++)
 	{
 		EN3[i].set(Spike, k * 16, 15 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN4(2);
-	for (int i = 0, k = 69; i < EN4.size(); i++)
+	/*for (int i = 0, k = 69; i < EN4.size(); i++)
 	{
 		EN4[i].set(Spike, k * 16, 15 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY_SP> EN5(3);
-	for (int i = 0, k = 62; i < EN5.size(); i++)
+	/*for (int i = 0, k = 62; i < EN5.size(); i++)
 	{
 		EN5[i].set(Small_spike, k * 16, 15.5 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY_SP> EN6(3);
-	for (int i = 0, k = 66; i < EN6.size(); i++)
+	/*for (int i = 0, k = 66; i < EN6.size(); i++)
 	{
 		EN6[i].set(Small_spike, k * 16, 15.5 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN7(1);
-	for (int i = 0, k = 88; i < EN7.size(); i++)
+	/*for (int i = 0, k = 88; i < EN7.size(); i++)
 	{
 		EN7[i].set(Spike, k * 16, 15 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN8(2);
-	for (int i = 0, k = 94; i < EN8.size(); i++)
+	/*for (int i = 0, k = 94; i < EN8.size(); i++)
 	{
 		EN8[i].set(Spike, k * 16, 14 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY_SP> EN9(3);
-	for (int i = 0, k = 101; i < EN9.size(); i++)
+	/*for (int i = 0, k = 101; i < EN9.size(); i++)
 	{
 		EN9[i].set(Small_spike, k * 16, 15.5 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY_SP> EN10(25);
-	for (int i = 0, k = 136; i < EN10.size(); i++)
+	/*for (int i = 0, k = 136; i < EN10.size(); i++)
 	{
 		EN10[i].set(Small_spike, k * 16, 15.5 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN11(1);
-	for (int i = 0, k = 135; i < EN11.size(); i++)
+	/*for (int i = 0, k = 135; i < EN11.size(); i++)
 	{
 		EN11[i].set(Spike, k * 16, 7 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN12(4);
-	for (int i = 0, k = 146; i < EN12.size(); i++)
+	/*for (int i = 0, k = 146; i < EN12.size(); i++)
 	{
 		EN12[i].set(Spike, k * 16, 11 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN13(4);
-	for (int i = 0, k = 156; i < EN13.size(); i++)
+	/*for (int i = 0, k = 156; i < EN13.size(); i++)
 	{
 		EN13[i].set(Spike, k * 16, 10 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN14(4);
-	for (int i = 0, k = 167; i < EN14.size(); i++)
+	/*for (int i = 0, k = 167; i < EN14.size(); i++)
 	{
 		EN14[i].set(Spike, k * 16, 11 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN15(4);
-	for (int i = 0, k = 177; i < EN15.size(); i++)
+	/*for (int i = 0, k = 177; i < EN15.size(); i++)
 	{
 		EN15[i].set(Spike, k * 16, 10 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN16(1);
-	for (int i = 0, k = 184; i < EN16.size(); i++)
+	/*for (int i = 0, k = 184; i < EN16.size(); i++)
 	{
 		EN16[i].set(Spike, k * 16, 11 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN17(1);
-	for (int i = 0, k = 185; i < EN17.size(); i++)
+	/*for (int i = 0, k = 185; i < EN17.size(); i++)
 	{
 		EN17[i].set(Spike, k * 16, 12 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN18(1);
-	for (int i = 0, k = 194; i < EN18.size(); i++)
+	/*for (int i = 0, k = 194; i < EN18.size(); i++)
 	{
 		EN18[i].set(Spike, k * 16, 12 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN19(1);
-	for (int i = 0, k = 201; i < EN19.size(); i++)
+	/*for (int i = 0, k = 201; i < EN19.size(); i++)
 	{
 		EN19[i].set(Spike, k * 16, 13 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN20(1);
-	for (int i = 0, k = 216; i < EN20.size(); i++)
+	/*for (int i = 0, k = 216; i < EN20.size(); i++)
 	{
 		EN20[i].set(Spike, k * 16, 9 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY_SP> EN21(29);
-	for (int i = 0, k = 215; i < EN21.size(); i++)
+	/*for (int i = 0, k = 215; i < EN21.size(); i++)
 	{
 		EN21[i].set(Small_spike, k * 16, 15.5 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY> EN22(3);
-	for (int i = 0, k = 219; i < EN22.size(); i++)
+	/*for (int i = 0, k = 219; i < EN22.size(); i++)
 	{
 		EN22[i].set(Spike, k * 16, 15 * 16);
 		k--;
-	}
+	}*/
 	vector<ENEMY_WALL> Wall(1);
 	//Wall[0].set(671, 15 * 16);
 
@@ -571,7 +576,7 @@ int main()
 		if (Keyboard::isKeyPressed(Keyboard::Up))	if (Mario.onGround) { Mario.dy = -0.19; Mario.onGround = false; }
 
 
-
+		P.update(time);
 		if (Mario.onGround)
 		{
 			/**/
@@ -681,6 +686,16 @@ int main()
 		for (int i = 0; i < Wall.size(); i++)
 		{
 			Wall[i].update(time);
+		}
+		if (Mario.rect.intersects(P.rect))
+		{
+			window.clear(Color::White);
+			background.setColor(Color::Green);
+			window.draw(background);
+			gover.setPosition(100, 15);
+			window.draw(gover);
+			window.display();
+			goto breakpoint;
 		}
 		for (int i = 0; i < J1.size(); i++)
 		{
@@ -966,6 +981,7 @@ int main()
 		{
 			window.draw(J1[i].sprite);
 		}
+		window.draw(P.sprite);
 		window.display();
 	}
 }
